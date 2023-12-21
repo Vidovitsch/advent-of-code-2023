054e3cd54d0576c83bd846f4c76c8d7924c2c40b
David de Bekker <david.de.bekker@gmail.com>
2023-12-21 22:17:33 +0100

    Solve day 1 part 1

--- solutions/1/1.py
+++ solutions/1/1.py
@@ -1,4 +1,5 @@
 import pathlib
+import re
 from typing import List
 
 from advent_helper.decorators import process_puzzle_input
@@ -13,10 +14,15 @@ def process_input(input: List[str]) -> PuzzleInput:
 
 @process_puzzle_input(process_input)
 def solve(input: PuzzleInput) -> int:
-  return 0
+  sum = 0
+  for line in input:
+    numbers = re.findall(r'\d', line)
+    sum += int(numbers[0] + numbers[-1])
+
+  return sum
 
 if __name__ == '__main__':
   (Puzzle('Day 1: Trebuchet?! (part 1)', CURRENT / 'input.txt')
     .add_test({ 'input_path': CURRENT / 'test.txt', 'expected_result': 142 })
-    .add_test({ 'input_path': CURRENT / 'input.txt', 'expected_result': "?" })
+    .add_test({ 'input_path': CURRENT / 'input.txt', 'expected_result': 54951 })
     .solve(solve))
